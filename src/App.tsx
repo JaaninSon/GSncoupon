10d7d3929b41dc2913bc670ac43ba1e76c28ef18
Bit-na25 <bnlove2025@gmail.com>
2025-04-12 12:13:12 +0900

    fix conflict

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import MainPage from "./pages/Main/MainPage";
 import { FilterProvider } from "./pages/detail/filter/FilterContext";
 import Nav from "./pages/Main/Nav";
+import CartPage from "./pages/cart/CartPage";
 
 function App() {
     return (
         <BrowserRouter>
             <FilterProvider>
                 <Nav />
-                <Routes>{/* <Route path="/" element={<MainPage />} /> */}</Routes>
+                <Routes>
+                    {/* <Route path="/" element={<MainPage />} /> */}
+                    <Route path="/cart" element={<CartPage />} />
+                    {/* <div className="app">
+                    <div className="container">
+                        <Filter />
+                        <div className="product-section">
+                            <ProductList />
+                        </div>
+                    </div>
+                </div> */}
+                </Routes>
             </FilterProvider>
         </BrowserRouter>
     );
