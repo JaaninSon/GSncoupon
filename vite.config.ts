33d05e7eb361369b68387a2256ba4c6bc2430dc9
JaaninSon <jaan1ove@naver.com>
2025-04-17 17:44:57 +0900

    retry build setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-import * as fs from "fs";
+import { resolve } from "path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const tsconfig = JSON.parse(fs.readFileSync("./tsconfig.app.json", "utf-8"));
-const tsconfigRaw = tsconfig.compilerOptions;
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
         },
-    },
-    esbuild: {
-        tsconfigRaw,
     },
 });
