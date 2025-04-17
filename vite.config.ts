7f50133b03b2b713acb2678a3e9358bf161bcf96
JaaninSon <jaan1ove@naver.com>
2025-04-17 18:48:38 +0900

    fix: bypass TS default import error in vite.config for successful deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-// vite.config.ts
 import { defineConfig } from "vite";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
